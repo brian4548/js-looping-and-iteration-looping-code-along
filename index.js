565227bfb1a84c96589eee5b499fce96512996c2
brian4548 <brianr4548@gmail.com>
2021-02-03 14:35:47 -0600

    commit tree cd7bd9a626b56979448b94881084c196de9d4156

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let array = ["Ada", "Brendan", "Ali"];
+let string = "birthday"
+
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(integer){
+    while(integer >= 0){
+        console.log(integer--)
+    }
+}
